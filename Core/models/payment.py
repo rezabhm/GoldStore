8d990eeb74496e9839a8dc6a1cea3d94a82a0ebc
reza <rezabhm50@gmail.com>
2023-12-06 15:47:38 +0330

    correct payment models

--- Core/models/payment.py
+++ Core/models/payment.py
@@ -8,6 +8,13 @@ class PaymentTransactions(models.Model):
     payment_date = models.DateTimeField()
     user = models.ForeignKey(User, on_delete=models.PROTECT)
     money_amount = models.FloatField(default=0.0)
+    status = models.CharField(max_length=20,default=' در انتظار واریز وجه', choices=(
+
+        (' در انتظار واریز وجه', ' در انتظار واریز وجه'),
+        ('واریز موفقیت آمیز', 'واریز موفقیت آمیز'),
+        ('واریز با حطا', 'واریز با حطا'),
+
+    ))
 
     def __str__(self):
-        return self.pk
+        return str(self.pk)
