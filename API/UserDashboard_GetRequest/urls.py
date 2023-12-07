d951e7adfb1ad431957ab0178a627d12332bec6e
reza <rezabhm50@gmail.com>
2023-12-07 10:10:23 +0330

    add wallet-data api to user-dashboard-get request

--- API/UserDashboard_GetRequest/urls.py
+++ API/UserDashboard_GetRequest/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
+from API.UserDashboard_DeskPage.views import WalletData
 from API.UserDashboard_GetRequest.views import GetRequestMoney, GetRequestGold
 
 urlpatterns = [
 
     path('get-request-money/', GetRequestMoney.as_view()),
     path('get-request-gold/', GetRequestGold.as_view()),
+    path('wallet-data/', WalletData.as_view()),
 
 ]
