8986f9c3020e439e33f0d01f2327b1bd892e97e0
reza <rezabhm50@gmail.com>
2023-12-07 13:26:45 +0330

    correct payment models

--- Core/models/payment.py
+++ Core/models/payment.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from datetime import datetime
 
 
 class PaymentTransactions(models.Model):
@@ -8,7 +7,7 @@ class PaymentTransactions(models.Model):
     payment_date = models.DateTimeField()
     user = models.ForeignKey(User, on_delete=models.PROTECT)
     money_amount = models.FloatField(default=0.0)
-    status = models.CharField(max_length=20,default=' در انتظار واریز وجه', choices=(
+    status = models.CharField(max_length=20, default=' در انتظار واریز وجه', choices=(
 
         (' در انتظار واریز وجه', ' در انتظار واریز وجه'),
         ('واریز موفقیت آمیز', 'واریز موفقیت آمیز'),
