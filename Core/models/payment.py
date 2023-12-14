c413bc7d0960c1b75333137005844561c77f817b
reza <rezabhm50@gmail.com>
2023-12-14 11:10:31 +0330

    update payment models

--- Core/models/payment.py
+++ Core/models/payment.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 class PaymentTransactions(models.Model):
 
     payment_date = models.DateTimeField()
+    payment_id = models.CharField(default='11111', null=True, max_length=20)
     user = models.ForeignKey(User, on_delete=models.PROTECT)
     money_amount = models.FloatField(default=0.0)
     status = models.CharField(max_length=20, default=' در انتظار واریز وجه', choices=(
